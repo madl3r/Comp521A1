ac3ecaf8449d3c6e85e7a7451a9f9009eac8dbb1
Matthew Adler <madler72@gmail.com>
2014-01-28 23:54:56 -0500

    Initial Push, first person shooting is in.

---
+++ A1/Assets/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+	
+	
+	public GameObject theExplosion;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter(Collision c)
+	{
+		Debug.Log ("I'm dead");
+		Destroy(gameObject);
+	}
+	
+}
+
+
