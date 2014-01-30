d309666f10fafdb6e112d1989c7a6584d7fd5423
Matthew Adler <madler72@gmail.com>
2014-01-30 17:07:56 -0500

    More implementation on CoD

---
+++ A1/Assets/Scripts/PlaceRemoveCoat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceRemoveCoat : MonoBehaviour {
+
+	public GameObject theCloak;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Instantiate(theCloak);
+	}
+}
---
+++ A1/Assets/Scripts/SlideDoorTrigger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideDoorTrigger : MonoBehaviour {
+	
+	public GameObject theDoor;
+	public GameObject invisDoor;
+	private Vector3 startScale = new Vector3 (1, 1, 1);
+	private Vector3 endScale = new Vector3 (20, 1, 1);
+	private float i;
+	private bool entered;
+
+	// Use this for initialization
+	void Start () {
+		i = 0.0f;
+		entered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!entered)
+		{
+			entered = true;
+			StartCoroutine(CloseDoor());
+			invisDoor.collider.enabled = true;
+
+		}
+	}
+
+
+	IEnumerator CloseDoor()
+	{
+		while(i < 1.0f)
+		{
+			i += Time.deltaTime;
+			//Debug.Log(i);
+			theDoor.transform.localScale = Vector3.Lerp(startScale, endScale, i);
+			yield return new WaitForSeconds(0.0005f);
+		}
+		yield break;
+	}
+}
